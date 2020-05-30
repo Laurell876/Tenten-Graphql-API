41f98b6e3fb7855e68b9b824ee5740884f89a95b
Laurell Seville <laurellseville2000@gmail.com>
2020-05-29 18:45:59 -0700

    added dynamic url

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.use(cors())
 
 connect
   .then(() => {
-    app.listen({ port: 4000 }, () =>
+    app.listen({ port: process.env.PORT || 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 );
   })
