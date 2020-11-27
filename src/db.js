4689baf0ac5738cc1e0e1befe7ccb80e07e253fc
Laurell Seville <laurellseville2000@gmail.com>
2020-11-26 21:46:34 -0500

    using a working db

--- src/db.js
+++ src/db.js
@@ -1,7 +1,12 @@
 
 const mongoose = require("mongoose");
 
-const MONGO_CONNECTION = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@tenten-pp23z.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+
+// This is currently connected to the db i used for ayuda
+const MONGO_CONNECTION =`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ayuda.pp23z.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+
+
+//`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@tenten-pp23z.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 
 //const MONGO_CONNECTION = `mongodb://127.0.0.1:27017/${process.env.MONGO_DB}`
 
